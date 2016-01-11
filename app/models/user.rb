7b95a476b200c1f22efa19ad58e0f1c5c0494770
Hugo Gonzalez <gonzalez.hugo.c@gmail.com>
2016-01-10 18:53:04 -0800

    Add first and last name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :pins
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+
+	def fullname
+	  [firstname, lastname].join(' ')
+	end
 end
